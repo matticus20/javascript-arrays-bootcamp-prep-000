094719c8b2cdb7a027a6ef121f18c378d8d38284
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 21:31:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,14 +3,12 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var countries = ["France", "Canada", "Japan"];
 var otherCountry = "Brazil"
 function addElementToBeginningOfArray(countries, otherCountry) {
-  
   var moreCountries = [otherCountry, ...countries]
   return moreCountries
 }
 
 function destructivelyAddElementToBeginningOfArray(countries, otherCountry) {
-  var countries = ["France", "Canada", "Japan"];
-  var otherCountry = "Brazil"
+  return countries.unshift(otherCountry)
 }
 
 var cities = ["London", "Paris", "Tokyo"]
@@ -20,9 +18,8 @@ function addElementToEndOfArray(cities, otherCity) {
   return moreCities
 }
 
-
-function destructivelyAddElementToEndOfArray() {
-  
+function destructivelyAddElementToEndOfArray(cities, otherCity) {
+  return cities.push(otherCity)
 }
 
 function accessElementInArray(vehicles, index) {
@@ -31,3 +28,22 @@ function accessElementInArray(vehicles, index) {
   return vehicles[parseInt(index)]
 }
 
+var days = ["Monday", "Tuesday", "Wednesday"]
+function destructivelyRemoveElementFromBeginningOfArray(days) {
+  return days.shift(0)
+}
+
+function removeElementFromBeginningofArray(days) {
+  var newDays = days.slice(0)
+  return newDays
+}
+
+var iceCreams = ["chocolate", "vanilla", "raspberry"]
+function destructivelyRemoveElementFromEndOfArray(iceCreams) {
+  return iceCreams.pop(2)
+}
+
+function removeElementFromEndOfArray() {
+  var newIceCreams = iceCreams.slice(0, iceCreams.length - 1)
+  return newIceCreams
+}
