ae46782532d19eb422619c0bfe40ab6a9b0049bb
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 22:15:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,8 @@ function destructivelyRemoveElementFromBeginningOfArray(days) {
 }
 
 function removeElementFromBeginningofArray(days) {
-  return days.slice(1)
+  newDays = days.slice(1)
+  return newDays
 }
 
 var iceCreams = ["chocolate", "vanilla", "raspberry"]
